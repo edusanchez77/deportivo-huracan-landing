14d8268ee63df685227aea20dc4b93efc4226ca0
Eduardo Sanchez - Nerdev <eduardosanchez77@gmail.com>
2024-11-04 10:27:25 -0300

    Agregué cambios a mi repo!

---
+++ src/const/teams.ts
@@ -0,0 +1,54 @@
+import type { Teams } from "@/types/Teams";
+
+export const TEAMS: Array<Teams> = [
+    {
+        name: 'Deportivo Huracán',
+        shortName: 'HUR',
+        games: 12,
+        won: 6,
+        tied: 2,
+        lost: 4,
+        goalsInFavor: 31,
+        goalsAgainst: 19
+    },
+    {
+        name: 'Sportivo 9 de Julio',
+        shortName: '9JU',
+        games: 12,
+        won: 5,
+        tied: 3,
+        lost: 4,
+        goalsInFavor: 28,
+        goalsAgainst: 21
+    },
+    {
+        name: 'Agrario de Corralito',
+        shortName: 'AGR',
+        games: 12,
+        won: 5,
+        tied: 3,
+        lost: 4,
+        goalsInFavor: 32,
+        goalsAgainst: 22
+    },
+    {
+        name: 'Atlético Ascasubi',
+        shortName: 'ASC',
+        games: 12,
+        won: 2,
+        tied: 2,
+        lost: 8,
+        goalsInFavor: 12,
+        goalsAgainst: 23
+    },
+    {
+        name: 'Deportivo Independiente (Río III)',
+        shortName: 'IND',
+        games: 12,
+        won: 3,
+        tied: 5,
+        lost: 4,
+        goalsInFavor: 12,
+        goalsAgainst: 23
+    }
+]
---
+++ src/types/Teams.ts
@@ -0,0 +1,10 @@
+export interface Teams {
+    name: String,
+    shortName: String,
+    games: number,
+    won: number,
+    tied: number,
+    lost: number,
+    goalsInFavor: number,
+    goalsAgainst: number
+}
