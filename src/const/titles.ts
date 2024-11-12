d98f28a87e804964d947e22c76dc670f6383714c
Eduardo Sanchez <eduardosanchez77@gmail.com>
2024-11-12 08:09:17 -0300

    Se agregaron todas las secciones

---
+++ src/const/titles.ts
@@ -0,0 +1,57 @@
+export const TITLES = [
+    {
+        tournament: 'Primera División',
+        date: '1968',
+        division: 1
+    },
+    {
+        tournament: 'Primera División',
+        date: '1969',
+        division: 1
+    },
+    {
+        tournament: 'Primera División',
+        date: '1970',
+        division: 1
+    },
+    {
+        tournament: 'Primera División',
+        date: '1971',
+        division: 1
+    },
+    {
+        tournament: 'Primera División',
+        date: '1972',
+        division: 1
+    },
+    {
+        tournament: 'Primera División',
+        date: '1974',
+        division: 1
+    },
+    {
+        tournament: 'Primera División',
+        date: '1975',
+        division: 1
+    },
+    {
+        tournament: 'Torneo Apertura',
+        date: '2015',
+        division: 1
+    },
+    {
+        tournament: 'Torneo Clausura',
+        date: '2017',
+        division: 1
+    },
+    {
+        tournament: 'Segunda División',
+        date: '1991',
+        division: 2
+    },
+    {
+        tournament: 'Segunda División',
+        date: '2013',
+        division: 2
+    }
+]
